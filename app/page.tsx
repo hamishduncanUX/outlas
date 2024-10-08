039d54a114e4e77d3ae2938ac2532fa246656252
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-10-08 09:31:48 +0100

    code added to landing page to confirm database connection work

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
+import { sql } from "@vercel/postgres";
 
+export default async function Cart(
+  
+): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * FROM testTable`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.name} - {row.role}
+        </div>
+      ))}
+    </div>
+  );
+}
+/*
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -99,3 +116,7 @@ export default function Home() {
     </div>
   );
 }
+
+import { sql } from "@vercel/postgres";
+*/
+
