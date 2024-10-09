ce1375aa3bfb0c015ffb42e97338b1774c6acb90
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-10-09 08:07:02 +0100

    map.tsx updated to publish values of longitude, latitude and zoom degree assigned to state

--- app/ui/mapbox/map.tsx
+++ app/ui/mapbox/map.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -18,17 +18,38 @@ an error
 
 */
 
+const INITIAL_CENTER = [
+  -74.0242,
+  40.6941
+]
+const INITIAL_ZOOM = 10.12
+
 export default function Map() {
 
     const mapRef = useRef()
-    const mapContainerRef = useRef()    
+    const mapContainerRef = useRef()
+    
+    const [center, setCenter] = useState(INITIAL_CENTER)
+    const [zoom, setZoom] = useState(INITIAL_ZOOM)
 
     mapboxgl.accessToken=process.env.NEXT_PUBLIC_MAPBOX_TOKEN || '';   
 
     useEffect(() => {        
         mapRef.current = new mapboxgl.Map({
           container: mapContainerRef.current,
+          center: center,
+          zoom: zoom
         });
+
+        mapRef.current.on('move', () => {
+        // get the current center coordinates and zoom level from the map
+        const mapCenter = mapRef.current.getCenter()
+        const mapZoom = mapRef.current.getZoom()
+
+        // update state
+        setCenter([ mapCenter.lng, mapCenter.lat ])
+        setZoom(mapZoom)
+        })
     
         return () => {
           mapRef.current.remove()
@@ -37,6 +58,9 @@ export default function Map() {
 
   return (
     <>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
       <div id='map-container' ref={mapContainerRef}/>
     </>    
   );
