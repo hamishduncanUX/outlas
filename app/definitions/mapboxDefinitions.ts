20997fa54a7089390a565e944f083b3c2061f414
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-10-09 14:05:58 +0100

    resorts type created in app/definitions/mapBoxDefinitions

---
+++ app/definitions/mapboxDefinitions.ts
@@ -0,0 +1,18 @@
+export type Resorts = {
+    Name: string,
+    Slug: string,
+    Item_id: string,
+    Resorts: string,
+    Gear_for_rent: string,
+    Email_address: string,
+    Profile_picture: string,
+    Description: string,
+    Phone_number: string,
+    Website: string,
+    Social_media_link: string,
+    Reason_signed_up: string,
+    Address: string,
+    Lat: number,
+    Long: number,
+    Regions: string
+}
